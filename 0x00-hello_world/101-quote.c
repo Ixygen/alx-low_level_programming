6153ea308b29db5272bfd756f9430ad63ba26c04
Ixygen <naivenyabinya@gmail.com>
2022-07-04 08:59:42 -0700

    use write instead of printf and show standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,14 @@
 #include <unisitd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful
+ * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
- *
  *   followed by a new line, to the standard error.
  *
  *   Return: 1 if success.
  */
-
 int main(void)
 {
-	write("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n"'59);
 	return (1);
 }
