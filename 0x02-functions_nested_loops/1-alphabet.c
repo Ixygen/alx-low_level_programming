17870f194f0dbb3dc96626c1b1b403e21bff1f22
Ixygen <naivenyabinya@gmail.com>
2022-07-05 14:00:12 -0700

    print lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * lowercaseAlphabets - print alphabets in lowercase
+ *
+ * Description: print lowercaseAlphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+void lowercaseAlphabets(void)
+{
+	int i;
+
+	for (i = '97'; i <= '122'; ++i)
+	{
+
+	_putchar(i);
+
+	}
+	_putchar('\n');
+}
