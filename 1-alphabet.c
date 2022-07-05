6cd2c7bc8488d38d41ef0be62316b22c5037162e
Ixygen <naivenyabinya@gmail.com>
2022-07-05 14:11:22 -0700

    print lowercase alphabets

---
+++ 1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * lowercaseAlphabets - print lower case alphabets
+ *
+ * Return: 0 (Success)
+ */
+
+void lowercaseAlphabets(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
