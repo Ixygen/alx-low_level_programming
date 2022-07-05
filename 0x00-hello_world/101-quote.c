4aa575e5f2ddf2921fb134c0b0bb0b3ab2b5484e
Ixygen <naivenyabinya@gmail.com>
2022-07-04 21:14:46 -0700

    review

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unisitd.h>
+#include <unistd.h>
 
 /**
  * main - prints exactly "and that piece of art is useful"
@@ -9,6 +9,7 @@
  *
  *   Return: 1 if success.
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
