6a1a1cbc9910f4417a865df57663c776ce1a5124
Ixygen <naivenyabinya@gmail.com>
2022-07-07 04:44:21 -0700

    print using write and include standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
  *
- *   followed by a new line, to the standard error.
+ * followed by a new line, to the standard error.
  *
- *   Return: 1 if success.
+ * Return: 1 if success.
  */
 
 int main(void)
