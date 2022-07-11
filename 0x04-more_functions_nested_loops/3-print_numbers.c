bbcac1f8769a0cd2bea9bf1abf3f05272f43e8e1
Ixygen <naivenyabinya@gmail.com>
2022-07-11 14:28:02 -0700

    print digits except 2 and 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0 to 9
+ * followed by a new line
+ * @n: int type to represent the digits
+ * Return: value of n
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	n = -9;
+	while (n < 0, n++)
+	{
+		_putchar(-n);
+	}
+}
