e0c92a9b98be6444749439b90b9aa20259ab9655
Ixygen <naivenyabinya@gmail.com>
2022-07-11 13:00:51 -0700

    check for uppercase character c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase character
+ * @c -int type character
+ * Description: check for uppercase character c.
+ * Return: 1 if the c character is uppercase otherwise return 0
+ */
+
+int _isupper(int c)
+{
+	while (c = 'C')
+	{
+		_putchar(1);
+	}
+	else
+	{
+		_putchar(0);
+	}
+}
