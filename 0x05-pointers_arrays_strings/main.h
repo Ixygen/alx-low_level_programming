cc76503b87ff62fcd406804ed173070cddcabea8
Ixygen <naivenyabinya@gmail.com>
2022-07-12 07:28:14 -0700

    5

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@ void reset_to_98(int *n);
 void swao_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-void rev_string(char *s);
 void print_rev(char *s);
+void rev_string(char *s);
 
 #endif
