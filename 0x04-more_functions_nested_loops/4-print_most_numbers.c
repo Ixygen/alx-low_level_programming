4c18dc0b11032215dccceced0ec12cace168e5e0
Ixygen <naivenyabinya@gmail.com>
2022-07-14 02:55:24 -0700

    4 more nested loops c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print the numbers, from 0 to 9,
+ * print_most_numbers - prints the numbers, from 0 to 9,
  * except 2 and 4, followed by a new line
  */
 
@@ -16,5 +16,6 @@ void print_most_numbers(void)
 			_putchar(i + '0');
 		}
 	}
+
 	_putchar('\n');
 }
