5c1c1ac0f3f78f86d0288ac45fe66295093e3851
Ixygen <naivenyabinya@gmail.com>
2022-07-28 18:22:59 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,10 +2,10 @@
 
 /**
  * _strncat - concatenates two strings,
- * @dest: destination
- * @src: source.
- * @n: amount of bytes used from src.
- * Return: the pointer to dest.
+ * @dest: param 1
+ * @src: param 2
+ * @n: param 3
+ * Return: string
  */
 char *_strncat(char *dest, char *src, int n)
 {
